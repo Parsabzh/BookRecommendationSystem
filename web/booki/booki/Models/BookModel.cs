bc3c12ef771f22b2d4ad96d5b3244978ab9b944a
parsabzh <parsabzh@gmail.com>
2023-07-12 21:46:31 +0200

    add getText method

---
+++ web/booki/booki/Models/BookModel.cs
@@ -0,0 +1,20 @@
+﻿namespace booki.Models
+{
+    public class BookModel
+    {
+        public BookModel(string name, string isbn, string author, string year, string getText)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Isbn = isbn ?? throw new ArgumentNullException(nameof(isbn));
+            Author = author ?? throw new ArgumentNullException(nameof(author));
+            Year = year ?? throw new ArgumentNullException(nameof(year));
+            GetText = getText ?? throw new ArgumentNullException(nameof(getText));
+        }
+
+        public string Name { get; set; }
+        public string Isbn { get; set; }
+        public string Author { get; set; }
+        public string Year { get; set; }
+        public string GetText { get; set; }
+    }
+}
